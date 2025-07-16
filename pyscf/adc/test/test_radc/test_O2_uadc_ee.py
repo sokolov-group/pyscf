b6ffef82cb2320c79f96f587ba604ca4de06b163
Terrence Stahl <stahl.225@osu.edu>
2025-07-16 17:39:17 -0400

    added radc_ee test, need to think about adding a spin square get flag

---
+++ pyscf/adc/test/test_radc/test_O2_uadc_ee.py
@@ -0,0 +1,103 @@
+# Copyright 2014-2019 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Terrence Stahl <terrencestahl1@@gmail.com>
+#         Alexander Sokolov <alexander.y.sokolov@gmail.com>
+#
+
+import unittest
+import numpy as np
+from pyscf import gto
+from pyscf import scf
+from pyscf import adc
+
+def setUpModule():
+    global mol, mf, myadc
+    
+    basis = 'cc-pVDZ'
+    r = 1.215774
+    
+    mol = gto.Mole()
+    mol.verbose = 0
+    mol.atom = [
+        ['O', ( 0., 0.    , -r/2)],
+        ['O', ( 0., 0., r/2)],]
+    mol.basis = {'O': basis,}
+    
+    mol.spin = 2
+    mol.symmetry = True
+    mol.build()
+    
+    mf = scf.ROHF(mol)
+    mf.conv_tol = 1e-12
+    mf.scf()
+
+
+    myadc = adc.ADC(mf)
+
+def tearDownModule():
+    global mol, mf, myadc
+    del mol, mf, myadc
+
+
+class KnownValues(unittest.TestCase):
+
+    def test_ee_adc2(self):
+        myadc.method = "adc(2)"
+
+        myadc.method_type = "ee"
+        e,v,p,x = myadc.kernel(nroots=4)
+
+        self.assertAlmostEqual(e[0],0.2536868317, 6)
+        self.assertAlmostEqual(e[1],0.2536868317, 6)
+        self.assertAlmostEqual(e[2],0.2594502231, 6)
+        self.assertAlmostEqual(e[3],0.3621178740, 6)
+
+        self.assertAlmostEqual(p[0],0.00000000, 6)
+        self.assertAlmostEqual(p[1],0.00000000, 6)
+        self.assertAlmostEqual(p[2],0.00000000, 6)
+        self.assertAlmostEqual(p[3],0.18371407, 6)
+
+    def test_ee_adc2x(self):
+        myadc.method = "adc(2)-x"
+
+        e,v,p,x = myadc.kernel(nroots=4)
+
+        self.assertAlmostEqual(e[0],0.2388257320, 6)
+        self.assertAlmostEqual(e[1],0.2388257320, 6)
+        self.assertAlmostEqual(e[2],0.2434547828, 6)
+        self.assertAlmostEqual(e[3],0.3475847540, 6)
+
+        self.assertAlmostEqual(p[0],0.00000000, 6)
+        self.assertAlmostEqual(p[1],0.00000000, 6)
+        self.assertAlmostEqual(p[2],0.00000000, 6)
+        self.assertAlmostEqual(p[3],0.16973782, 6)
+
+    def test_ee_adc3(self):
+        myadc.method = "adc(3)"
+
+        e,v,p,x = myadc.kernel(nroots=4)
+
+        self.assertAlmostEqual(e[0],0.2073282139, 6)
+        self.assertAlmostEqual(e[1],0.2073282139, 6)
+        self.assertAlmostEqual(e[2],0.2123139019, 6)
+        self.assertAlmostEqual(e[3],0.3155432845, 6)
+
+        self.assertAlmostEqual(p[0],0.00000000, 6)
+        self.assertAlmostEqual(p[1],0.00000000, 6)
+        self.assertAlmostEqual(p[2],0.00000000, 6)
+        self.assertAlmostEqual(p[3],0.16727584, 6)
+if __name__ == "__main__":
+    print("EE calculations for different ADC methods for O2 molecule")
+    unittest.main()
