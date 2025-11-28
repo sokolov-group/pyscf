e06dee317dae6efd11af3efdbb31854aa2050344
Ning-Yuan Chen <cny003@outlook.com>
2025-11-28 14:46:00 -0500

    fix an error in ssfno in pbc.adc when frozen orbital is used

--- pyscf/pbc/adc/kadc_rhf_ea.py
+++ pyscf/pbc/adc/kadc_rhf_ea.py
@@ -1159,15 +1159,8 @@ def make_rdm1(adc,root=None,kptlist=None):
     log = logger.Logger(adc.stdout, adc.verbose)
 
     list_rdm1 = []
-    if root is None:
-        nroots = range(adc.U.shape[1])
-    else:
-        nroots = root
 
-    if kptlist is None:
-        kptlist = range(adc.nkpts)
-
-    for i in nroots:
+    for i in root:
         rdm1 = []
         for k, kshift in enumerate(kptlist):
             U = np.array(adc.U[k]).T.copy()
